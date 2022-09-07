c3f31809840f2bdd48eeafd9e67fbb2247f4ec01
fpellegrini <franziskapellegrini@web.de>
2022-09-07 12:05:15 +0200

    overview of analysis flow

---
+++ fp_motorImag_main.m
@@ -0,0 +1,11 @@
+%Analysis flow of motor imagery experiment.
+
+%preprocessing 
+fp_preprocess
+
+%source projection, aggregation within regions, calculating power and
+%testing it statistically 
+fp_powertest_MI
+
+%functional connectivity calculation and testing 
+fp_fc_test
